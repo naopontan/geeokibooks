3d25fa7480f958ae2f278431aa8b53788060504e
cieux1 <cieux1@gmail.com>
2012-07-25 20:40:31 +0900

    2 methods added

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,5 +10,28 @@ class Book < ActiveRecord::Base
     last_rental.rental?
   end
 
+  def on_rental_count
+  	count = 0
+  	rentals.each do |x|
+  		count += 1 if x.returned_at != nil
+  	end
+  	count
+  end
+
+  def left?
+  	if (quantity - on_rental_count) > 0
+  		return true
+  	else
+  		return false
+  	end
+  end
+
   # TODO: 「借りる」と「返す」のメソッド作る?メソッド名は?
+  def borrow
+ 
+  end
+
+  def put_back
+  	
+  end
 end
