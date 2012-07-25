d53813f562dc0534375532ab6045c7d35acfdc64
nao taka <naopontan@gmail.com>
2012-07-25 13:37:09 +0900

    「レンタルする」のテスト
    book_rental メソッドは内部で rental レコードを保存する。
    メソッド名は book_rental! がいいのかなぁ

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should book rental" do
+    assert users(:dummy1).book_rental(books(:tanoruby))
+  end
+
+  test "should not book rental" do
+    assert users(:dummy1).book_rental(books(:tanoruby)), '最後の1冊をレンタル'
+    assert_nil users(:dummy2).book_rental(books(:tanoruby)), '全てレンタル中なのでレンタルできない'
+  end
 end
