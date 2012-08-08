70d4aabfb08afd580c451b20613d905194cf1f81
cieux1 <cieux1@gmail.com>
2012-08-08 21:25:11 +0900

    changed method name

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,11 +3,11 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "should book rental" do
-    assert users(:dummy1).book_rental(books(:tanoruby))
+    assert users(:dummy1).borrow(books(:tanoruby))
   end
 
   test "should not book rental" do
-    assert users(:dummy1).book_rental(books(:tanoruby)), '最後の1冊をレンタル'
-    assert_nil users(:dummy2).book_rental(books(:tanoruby)), '全てレンタル中なのでレンタルできない'
+    assert users(:dummy1).borrow(books(:tanoruby)), '最後の1冊をレンタル'
+    assert_nil users(:dummy2).borrow(books(:tanoruby)), '全てレンタル中なのでレンタルできない'
   end
 end
